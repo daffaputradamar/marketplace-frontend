feaff7a0efd76159a48cd1e427ec11a74ef914bd
Faisal Wafa <faisalwafa10@gmail.com>
2019-11-01 15:39:40 +0700

    add layout penghasilan saya

--- src/pages/Usaha/PenghasilanSaya.jsx
+++ src/pages/Usaha/PenghasilanSaya.jsx
@@ -1,15 +1,51 @@
 import React from 'react'
-import { Grid, Segment } from 'semantic-ui-react'
+import { Grid, Segment, Dropdown, Divider } from 'semantic-ui-react'
 
 function PenghasilanSaya() {
+  const statusOptions = [
+    {
+      key: 'Pesanan Dibuat',
+      text: 'Pesanan Dibuat',
+      value: 'Pesanan Dibuat',
+    },
+    {
+      key: 'Pesanan Siap',
+      text: 'Pesanan Siap',
+      value: 'Pesanan Siap',
+    },
+    {
+      key: 'Pesanan Dibayar',
+      text: 'Pesanan Dibayar',
+      value: 'Pesanan Dibayar',
+    },
+  ]
   return (
     <React.Fragment>
       <Segment>
-        <Grid.Row>AAA</Grid.Row>
-        <Grid.Row>AAA</Grid.Row>
+        <Grid.Row>
+          <Grid columns={2}>
+            <Grid.Column width="7">
+              Status Pesanan :
+              <Dropdown
+                style={styles.marginLeft}
+                placeholder="Pilih Status Pesanan"
+                selection
+                options={statusOptions}></Dropdown>
+            </Grid.Column>
+            <Grid.Column width="7">Pesanan sampai dengan tanggal :</Grid.Column>
+          </Grid>
+        </Grid.Row>
+        <Divider />
+        <Grid.Row>Kriteria Utama :</Grid.Row>
       </Segment>
     </React.Fragment>
   )
 }
 
 export default PenghasilanSaya
+
+const styles = {
+  marginLeft: {
+    marginLeft: '20px',
+  },
+}
