3648cbaaf381d5103f37fc77009a69ac5b2741fc
Muhammad Fakhryan <fakhryanmuhammad@gmail.com>
2019-11-06 18:57:56 +0700

    add review functionality

---
+++ src/pages/DetailProduk/ItemUlasan.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+    Header,
+    Segment,
+  } from 'semantic-ui-react'
+import StarRatings from "react-star-ratings";
+
+function ItemUlasan() {
+    return (
+        <Segment>
+        <StarRatings
+          starRatedColor="blue"
+          numberOfStars={5}
+          name="rating"
+          starDimension="20px"
+          starSpacing="5px"
+        />
+        <Header as="h5" style={{marginTop:10}}>Oleh : </Header>
+        <p>AWassdad</p>
+      </Segment>
+    )
+}
+
+export default ItemUlasan
--- src/pages/Profil/ItemReview.jsx
+++ src/pages/Profil/ItemReview.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import { Container, Header, Grid, Image, TextArea, Button } from 'semantic-ui-react';
 import StarRatings from 'react-star-ratings';
+import axios from 'axios';
+import { HOSTNAME, UserContext } from '../../App';
 
 function ItemReview(props) {
+	const context = useContext(UserContext)
+	function changeRating(newRating, name) {
+		setRating(newRating)
+		
+	}
+	function addReview(idProduk) {
+		axios
+		.post(`${HOSTNAME}/review`, {
+			id_produk:idProduk,
+			rating,
+			komentar
+		},{
+			headers: { Authorization: `Bearer ${context.token}` },
+		}).then(res => {
+			setRating(0)
+			setKomentar('')
+		})
+	}
+	const [rating, setRating] = useState(0)
+	const [komentar, setKomentar] = useState('')
 	return (
 		<Container>
 			{props.transaksi.detail_transaksis.map((detail) => (
 				<div key={detail.id_detiltransaksi}>
 					<Grid>
 						<Grid.Row>
-							<Grid.Column width={9}>
+							<Grid.Column width={5}>
 								<Header as="h5">Nama Barang : {detail.produk.nama}</Header>
 							</Grid.Column>
-							<Grid.Column width={7}>
+							<Grid.Column width={11}>
 								<Grid>
 									<Grid.Column width={4}>
 										<p style={{ fontSize: 15 }}>Rating :</p>
 									</Grid.Column>
 									<Grid.Column width={11}>
 										<StarRatings
-											starRatedColor="blue"
+											starRatedColor="rgb(243, 156, 18)"
+											starHoverColor="rgb(243, 156, 18)"
 											numberOfStars={5}
 											name="rating"
+											rating = {rating}
+											changeRating = {changeRating}
 											starDimension="20px"
 											starSpacing="5px"
 										/>
@@ -31,13 +56,13 @@ function ItemReview(props) {
 						</Grid.Row>
 					</Grid>
 					<Grid>
-						<Grid.Column width={9}>
+						<Grid.Column width={5}>
 							<Image src="https://placeimg.com/120/120/any" size="small" />
 						</Grid.Column>
-						<Grid.Column width={7}>
+						<Grid.Column width={11}>
 							<p style={{ fontSize: 15 }}>Komentar</p>
-							<TextArea rows={3} style={{ width: 300, height: 80 }} />
-							<Button color="yellow" floated="right" style={{ marginRight: 50 }}>
+							<TextArea rows={3} style={{ width: 300, height: 80 }} onChange={(e)=>setKomentar(e.target.value)}/>
+							<Button color="yellow" floated="right" style={{ marginRight: 270 }} onClick={(e) => addReview(detail.produk.id_produk)}>
 								Kirim
 							</Button>
 						</Grid.Column>
